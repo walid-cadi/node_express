63ca0c6a3746a1e4ee240308e6e750e7b7d576f8
walid cadi <cadiwalid02@gmail.com>
2025-05-01 17:47:55 +0100

    connect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
-  res.sendFile('./view/home.html', { root: __dirname });
+  res.sendFile("./view/home.html", { root: __dirname });
 });
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
 });
+
+mongoose.connect("mongodb+srv://cadiwalid440:b0fBU7GKr1LgqmjP@cluster0.mxg1loi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+  });
+ })
+ .catch((err) => {
+   console.log(err);
+ });
