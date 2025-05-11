9f0af947044c5b4a6a756f5c78f22e69273c18a3
walid cadi <cadiwalid02@gmail.com>
2025-05-11 02:40:56 +0100

    Static files & auto refresh

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 const User = require("./models/userSchema");
 app.set("view engine", "ejs");
+app.use(express.static('public'))
+
+
+// Auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+ 
+ 
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+ 
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 app.get("/", (req, res) => {
   User.find()
