933a78e8f1e248de04984746d8a1fdd398745b91
walid cadi <cadiwalid02@gmail.com>
2025-05-14 20:03:31 +0100

    add user

--- app.js
+++ app.js
@@ -3,49 +3,65 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
-const Mydata = require("./models/userSchema");
+const User = require("./models/userSchema");
 app.set("view engine", "ejs");
-app.use(express.static('public'))
-
+app.use(express.static("public"));
 
 // Auto refresh
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'public'));
- 
- 
+liveReloadServer.watch(path.join(__dirname, "public"));
+
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
- 
+
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
     liveReloadServer.refresh("/");
   }, 100);
 });
 
-
-
-
-
-
-
+// get request
 app.get("/", (req, res) => {
-// result ==> array of objects
-res.render("index", {   });
-
+  // result ==> array of objects
+  res.render("index", {});
 });
 
 app.get("/user/add.html", (req, res) => {
-  res.render("user/add")
+  res.render("user/add");
 });
 
 app.get("/user/view.html", (req, res) => {
-  res.render("user/view")
+  res.render("user/view");
 });
 
 app.get("/user/edit.html", (req, res) => {
-  res.render("user/edit")
+  res.render("user/edit");
+});
+
+// post request
+app.post("/user/add", (req, res) => {
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    age: req.body.age,
+    country: req.body.country,
+    gender: req.body.gender
+  });
+  console.log(user);
+  user
+    .save()
+    .then(() => {
+      console.log("User added successfully");
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error adding user");
+    });
 });
 
 mongoose
@@ -60,5 +76,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-
--- models/userSchema.js
+++ models/userSchema.js
@@ -3,10 +3,16 @@ const Schema = mongoose.Schema;
 
 // Define the schema for the User model
 const userSchema = new Schema({
-  name: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  age: Number,
+  country: String,
+  gender: String,
 });
 // Create the User model using the schema
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("user", userSchema);
 
 // Export the User model for use in other files
 module.exports = User;
